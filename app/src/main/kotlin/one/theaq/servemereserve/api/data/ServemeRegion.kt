0a3a53759beb875c61140b8f9e1fdd86d2b0ddf6
Ivan Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-05-07 16:05:57 +0300

    append api to urls

--- app/src/main/kotlin/one/theaq/servemereserve/api/data/ServemeRegion.kt
+++ app/src/main/kotlin/one/theaq/servemereserve/api/data/ServemeRegion.kt
@@ -4,10 +4,10 @@ import java.net.URI
 import java.net.URL
 
 enum class ServemeRegion(val uri: URI) {
-    EU(URI.create("https://serveme.tf")),
-    NA(URI.create("https://na.serveme.tf")),
-    AU(URI.create("https://au.serveme.tf")),
-    SEA(URI.create("https://sea.serveme.tf"));
+    EU(URI.create("https://serveme.tf/api")),
+    NA(URI.create("https://na.serveme.tf/api")),
+    AU(URI.create("https://au.serveme.tf/api")),
+    SEA(URI.create("https://sea.serveme.tf/api"));
 
     val url: URL
         get() = uri.toURL()
