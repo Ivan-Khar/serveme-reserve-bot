e8f4b1f4abdc931231b8674844c740a18c161860
Ivan Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-05-07 14:03:13 +0300

    actions should be optionable

--- app/src/main/kotlin/one/theaq/servemereserve/api/data/ServemeActions.kt
+++ app/src/main/kotlin/one/theaq/servemereserve/api/data/ServemeActions.kt
@@ -1,9 +1,10 @@
 package one.theaq.servemereserve.api.data
 
 import java.net.URL
+import java.util.Optional
 
 data class ServemeActions(
-    val create: URL,
-    val patch: URL,
-    val delete: URL
+    val create: Optional<URL>,
+    val patch: Optional<URL>,
+    val delete: Optional<URL>
 ) {}
