31f262756578d1d0378f2d5ef4fffb69c78667d9
Ivan Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-05-07 14:38:47 +0300

    dont need a separate data class for cfgs

--- app/src/main/kotlin/one/theaq/servemereserve/api/data/ServemeConfig.kt
+++
@@ -1,6 +0,0 @@
-package one.theaq.servemereserve.api.data
-
-data class ServemeConfig(
-    val id: Int, // id
-    val file: String, // file
-) {}
---
+++ app/src/main/kotlin/one/theaq/servemereserve/api/data/ServemePersistent.kt
@@ -0,0 +1,6 @@
+package one.theaq.servemereserve.api.data
+
+data class ServemePersistent(
+    var maps: List<String>,
+    var cfgs: LinkedHashSet<String>
+) {}
