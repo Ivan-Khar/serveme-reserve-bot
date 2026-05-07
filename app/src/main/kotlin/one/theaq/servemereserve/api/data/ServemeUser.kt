70f267245cc2809a83524e2af921954ec967acd4
Ivan Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-05-07 14:02:05 +0300

    use Optional instead of nulls

--- app/src/main/kotlin/one/theaq/servemereserve/api/data/ServemeUser.kt
+++ app/src/main/kotlin/one/theaq/servemereserve/api/data/ServemeUser.kt
@@ -2,6 +2,7 @@ package one.theaq.servemereserve.api.data
 
 import java.time.Duration
 import java.time.LocalDateTime
+import java.util.Optional
 
 data class ServemeUser(
     val id: Int, // id
@@ -9,7 +10,7 @@ data class ServemeUser(
     val nickname: String, // nickname
     val name: String, // name // where do they get this value from
     val donator: Boolean, // donator
-    val donatorUntil: LocalDateTime?, // donator_until
+    val donatorUntil: Optional<LocalDateTime>, // donator_until
     val reservationsMade: Int, // reservations_made
     val totalReservationTime: Duration // total_reservation_seconds
 ) {}
