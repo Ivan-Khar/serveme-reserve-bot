f19cf999b6cb73e2d729395322acb74eb3c261f5
Ivan Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-05-07 14:24:30 +0300

    could just be booleans instead of URLs

--- app/src/main/kotlin/one/theaq/servemereserve/api/data/ServemeActions.kt
+++ app/src/main/kotlin/one/theaq/servemereserve/api/data/ServemeActions.kt
@@ -1,10 +1,7 @@
 package one.theaq.servemereserve.api.data
 
-import java.net.URL
-import java.util.Optional
-
 data class ServemeActions(
-    val create: Optional<URL>,
-    val patch: Optional<URL>,
-    val delete: Optional<URL>
+    val create: Boolean,
+    val patch: Boolean,
+    val delete: Boolean
 ) {}
